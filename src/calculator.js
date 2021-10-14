4da2c1ed3b8428229ef3eef0788b96765ab954b3
hashptl <ptlhiren535@gmail.com>
2021-10-14 16:20:44 +0530

    Added conditions and rules

--- src/calculator.js
+++ src/calculator.js
@@ -1,5 +1,25 @@
-function add(a, b) {
-    return a + b;
+function add(stringNumbers) {
+  let sum = 0;
+  let delimeter = /[,\n]/;
+  let numbers = stringNumbers.split(delimeter);
+
+  if (stringNumbers.subString(0, 2) === "//") {
+    let nums = stringNumbers.replace(/[^0-9]/, "");
+    numbers = nums.split("");
+  }
+  numbers.forEach((item) => {
+    let number = parseInt(item);
+
+    if (isNaN(number) || number > 1000) {
+      number = 0;
+    }
+
+    if (number < 0) {
+      throw "Adding negative numbers:" + number + "not allowed!";
+    }
+    sum += number;
+  });
+  return sum;
 }
 
 module.exports.add = add;
