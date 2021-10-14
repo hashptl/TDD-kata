7177359bd9d2f645d796612ac9613a5e05fb1207
hashptl <ptlhiren535@gmail.com>
2021-10-14 17:32:36 +0530

    Unit testing jest: Added few conditions, delimiters, exception.

--- test/calculator.test.js
+++ test/calculator.test.js
@@ -1,8 +1,27 @@
 const calculator = require("../src/calculator");
 
-const a = 13;
-const b = 5;
+test('Return 0 if output is ""', () => {
+  expect(add("")).toBe(0);
+});
 
-test("Add method should add numbers", () => {
-  expect(calculator.add(a, b)).toBe(18);
+test('Return 1 if input is "1"', () => {
+  expect(add("1")).toBe(1);
+});
+
+test('Return 3 if input is "1,2"', () => {
+  expect(add("1, 2")).toBe(6);
+});
+
+test("Return result for unknown numbers", () => {
+  expect(add("1, 2, 3")).toBe(6);
+});
+
+test("Handle new lines betweeen numbers", () => {
+  expect(add("1\n2, 3")).toBe(6);
+});
+
+test("Throw an exception if the input is a negative number", () => {
+  expect(() => {
+    add("-1, 2");
+  }).toThrow();
 });
